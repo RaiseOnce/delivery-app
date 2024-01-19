75c15ce0a82aec6a957a5e4816a4f0fab570c8c9
Otto Rahn <elias.yunusoff@gmail.com>
2024-01-19 16:01:55 +0500

    search input is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+import Header from '@/components/Header/Header'
 import styles from './page.module.scss'
 import Sidebar from '@/components/Sidebar/Sidebar'
+import Search from '@/components/Search/Search'
 
 export default function Menu() {
   return (
     <main className={styles.main}>
       <Sidebar />
-      <div className={styles.menu}>Menu</div>
+      <div className={styles.menu}>
+        <div className={styles.head}>
+          <Header>Меню</Header>
+          <Search placeholder="Введите блюдо или состав" />
+        </div>
+      </div>
     </main>
   )
 }
